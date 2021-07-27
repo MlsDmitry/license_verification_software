b3e0da4c05135749a5203c8e11464035d2877d5a
MlsDmitry <molokovich.dima@gmail.com>
2021-07-27 23:24:03 +0300

    app.py and __main__.py fixes

--- server/__main__.py
+++ server/__main__.py
@@ -1,5 +1,4 @@
 from .app import create_application
 
 app = create_application()
-
-app.run('0.0.0.0', port=8000, debug=True, auto_reload=True)
+app.run('0.0.0.0', port=31563, debug=False, workers=1)
