c0b5a99b2bfdb7f2374a2205f8933f6de1d466c2
MlsDmitry <molokovich.dima@gmail.com>
2021-08-17 02:16:01 +0300

    fixed tests

--- server/tests/test_license_verification.py
+++ server/tests/test_license_verification.py
@@ -1,10 +1,8 @@
-from server.app import create_application
-import pytest
 import sys
 from os import getcwd
-from os.path import dirname
 sys.path.append(getcwd())
-
+from server.app import create_application
+import pytest
 
 @pytest.fixture
 def app():
@@ -13,15 +11,15 @@ def app():
 
 
 @pytest.fixture
-def test_cli(loop, app, sanic_client):
+def server(loop, app, sanic_client):
     return loop.run_until_complete(sanic_client(app))
 
 
-async def test_index(test_cli):
-    resp = await test_cli.post('/api/login')
+async def test_index(server):
+    resp = await server.post('/api/login')
     assert resp.status_code == 422
 
 
-async def test_player(test_cli):
-    resp = await test_cli.post('/api/register')
+async def test_player(server):
+    resp = await server.post('/api/register')
     assert resp.status_code == 422
